156cfaa3c52bec3ad40d15ad3ec5f204a5cc50ce
Thiago Coelho <thiag0coelho@outlook.com>
2019-08-11 17:40:25 -0300

    Login, listing cards

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Login.css";
 
-import logo from "../assets/logo.svg"
+import api from "../services/api";
 
-function Login(){
-    return (
-        <img src={logo} alt="Tindev"></img>
-    );
+import logo from "../assets/logo.svg";
+
+function Login({ history }) {
+  const [username, setUsername] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const response = await api.post("/devs", {
+        username
+    });
+
+    const { _id } = response.data;
+
+    history.push(`/dev/${_id}`);
+  }
+
+  return (
+    <div className="login-container">
+      <form onSubmit={handleSubmit}>
+        <img src={logo} alt="Tindev" />
+        <input
+          placeholder="Digite seu usuário do GitHub"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
---
+++ frontend/src/pages/Main.js
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+
+import api from "../services/api";
+
+import "./Main.css";
+import logo from "../assets/logo.svg";
+import dislike from "../assets/dislike.svg";
+import like from "../assets/like.svg";
+
+export default function Main({ match }) {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function loadUsers() {
+      const response = await api.get("/devs", {
+        headers: {
+          user: match.params.id
+        }
+      });
+
+      setUsers(response.data);
+    }
+
+    loadUsers();
+  }, [match.params.id]);
+
+  async function handleLike(id) {
+    console.log("like " + id);
+  }
+  async function handleDislike(id) {
+    console.log("dislike " + id);
+  }
+
+  return (
+    <div className="main-container">
+      <img src={logo} alt="Tindev" />
+      <ul>
+        {users.map(user => (
+          <li key={user._id}>
+            <img src={user.avatar} alt="" />
+            <footer>
+              <strong>{user.name}</strong>
+              <p>{user.bio}</p>
+            </footer>
+            <div className="buttons">
+              <button type="button" onClick={() => handleDislike(user._id)}>
+                <img src={dislike} alt="Dislike" />
+              </button>
+              <button type="button" onClick={() => handleLike(user._id)}>
+                <img src={like} alt="Like" />
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
